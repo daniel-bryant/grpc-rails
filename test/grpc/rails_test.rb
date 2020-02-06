7bf3c4d31c7274223ef8f42b0c2de545eb1d20c2
daniel-bryant <bryant.daniel.j@gmail.com>
2020-02-05 18:44:33 -0800

    Hook up and test end-to-end DSL

--- test/grpc/rails_test.rb
+++ test/grpc/rails_test.rb
@@ -8,4 +8,27 @@ class GRPC::Rails::Test < ActiveSupport::TestCase
   test "grpc gem dependency" do
     assert_kind_of Class, GRPC::RpcServer
   end
+
+  test ".application" do
+    assert_instance_of GRPC::Rails::Application, GRPC::Rails.application
+  end
+
+  test "integration" do
+    GRPC::Rails.application.routes.draw do
+      protobuf :helloworld do
+        service :greeter do
+          rpc :hello
+          rpc :goodbye
+        end
+      end
+    end
+
+    assert_equal 1, GRPC::Rails.application.services.length
+    greeter = GRPC::Rails.application.services.first.new
+
+    reply = greeter.hello(Helloworld::HelloRequest.new(name: "Daniel"), {})
+    assert_equal "Hello Daniel!", reply.message
+
+    assert_raises(NoMethodError) { greeter.goodbye({}, {}) }
+  end
 end
